dcea57b1e96906ba390df80d3bd48069fb55cdf7
Mariusz Maciejczuk <mariusz.maciejczuk@intive.com>
2017-08-22 12:27:48 +0200

    first commit

---
+++ DesignPatterns/AbstractFactory/AbstractFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    public abstract class AbstractFactory
+    {
+        public virtual DbConnection Connection { get; set; }
+        public virtual DbCommand Cmmand { get; set; }
+    }
+}
